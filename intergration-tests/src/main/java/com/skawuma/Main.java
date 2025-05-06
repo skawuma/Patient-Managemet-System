f8fbae8913b650d1a2ac225f45c5ffd9248c48e0
skawuma <82976186+skawuma@users.noreply.github.com>
2025-05-06 18:18:56 -0400

    Added integration test

---
+++ intergration-tests/src/main/java/com/skawuma/Main.java
@@ -0,0 +1,12 @@
+package com.skawuma;
+
+/**
+* @package com.skawuma
+*@project Default (Template) Project
+*@author samuelkawuma
+*@date 5/6/25
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
